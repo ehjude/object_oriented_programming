e208f28458645bf75aa79fdf7b1bf0e293745e0b
Jude Fiorillo <jude.fiorillo@gmail.com>
2014-10-23 16:47:09 -0400

    rover can turn

--- exercise2.rb
+++ exercise2.rb
@@ -1,28 +1,67 @@
 class Rover
-	def initialize(x = 0, y = 0)
+	
+	attr_accessor :x, :y, :direction
+
+	def initialize(x = 0, y = 0, direction)
 		@x = x
 		@y = y
+		@direction = direction
 	end
 
-	def read_instruction(move_or_turn)
+	def read_instruction(instruction)
+		if instruction == "M"
+			move
+		elsif instruction == "L"
+			turn_left
+		elsif instruction == "R"
+			turn_right
+		else
+			puts "Instruction not recognized!"
+		end
 	end
 
-	def move(x, y)
-		@x += x
-		@y += y
+	def move
+		if direction == "N"
+			@y += 1
+		elsif direction == "W"
+			@x += -1
+		elsif direction == "S"
+			@y += -1
+		elsif direction == "E"
+			@x += 1						
+		end
 	end
 
-	def turn
+	def turn_left
+		if direction == "N"
+			@direction = "W"
+		elsif direction == "W"
+			@direction = "S"
+		elsif direction == "S"
+			@direction = "E"
+		elsif direction == "E"
+			@direction = "N"
+		end				
 	end
 
-	def to_s
-		"(#{@x}), (#{@y})"
+	def turn_right
+		if direction == "N"
+			@direction = "E"
+		elsif direction == "E"
+			@direction = "S"
+		elsif direction == "S"
+			@direction = "W"
+		elsif direction == "W"
+			@direction = "N"
+		end				
 	end
+
 end
 
-rover = Rover.new
-puts rover
-rover.move(1, 3)
-puts rover
+rover = Rover.new(0, 0, "N")
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
 
-
+puts "Enter an amount to move, or type L or R to turn left or right"
+user_instruction = gets.chomp
+rover.read_instruction(user_instruction)
+puts "Rover's current position is #{rover.x}, #{rover.y}, facing #{rover.direction}"
